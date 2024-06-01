4435b694b2921f0d06cf1c0dcadc1b2f3cf2e76d
AiTrinh303 <trong.trinh@dci-student.org>
2024-06-01 22:32:02 +0200

    Update reponsive for website

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,7 +1,8 @@
-import { useState, useContext } from 'react'
+import { useState, useContext, useRef } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import nav_dropdown from '../Assets/nav_dropdown.png'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../../Context/ShopContext'
 
@@ -9,6 +10,11 @@ function Navbar() {
 
   const [menu, setMenu] = useState("shop")
   const {getTotalCartItems} = useContext(ShopContext)
+  const menuRef = useRef();
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle('nav-menu-visible');
+    e.target.classList.toggle('open');
+  }
 
   return (
     <div className='navbar'>
@@ -18,7 +24,9 @@ function Navbar() {
         <p>SHOPPER</p>
       </div>
 
-      <ul className="nav-menu">
+      <img className ='nav-dropdown' src={nav_dropdown} alt="dropdown" onClick={dropdown_toggle}/>
+
+      <ul ref= {menuRef} className="nav-menu">
         <li onClick ={() => {setMenu('shop')}}>
           <Link style = {{textDecoration: 'none'}} to = '/'> Shop </Link> {menu === "shop" ?  <hr/> : <></>}
         </li>
